0811a949d3098be2d10a0160213cba585163cc14
Akshat-Garg7 <akshatgarg280503@gmail.com>
2025-03-30 21:22:22 +0530

    Final commit

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
         </div>
         <div>
           <div className={classes.hero}>
-            <h1>EazyEats Food for  Foodies</h1>
+            <h1>TasteTrail Food for  Foodies</h1>
             <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
@@ -25,25 +25,25 @@ export default function Home() {
         <section className={classes.section}>
           <h2>How it works</h2>
           <p>
-          EazyEats Food is a platform for foodies to share their favorite
+          TasteTrail Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-          EazyEats Food is a place to discover new dishes, and to connect
+          TasteTrail Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
 
         <section className={classes.section}>
-          <h2>Why EazyEats Food?</h2>
+          <h2>Why TasteTrail Food?</h2>
           <p>
-            EazyEats Food is a platform for foodies to share their favorite
+            TasteTrail Food is a platform for foodies to share their favorite
             recipes with the world. It&apos;s a place to discover new dishes, and to
             connect with other food lovers.
           </p>
           <p>
-            EazyEats Food is a place to discover new dishes, and to connect
+            TasteTrail Food is a place to discover new dishes, and to connect
             with other food lovers.
           </p>
         </section>
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -13,7 +13,7 @@ export default function MainHeader() {
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="A plate with food on it" priority />
-          EazyEats Food
+          TasteTrail Food
         </Link>
 
         <nav className={classes.nav}>
